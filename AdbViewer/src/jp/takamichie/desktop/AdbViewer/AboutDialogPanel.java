67a7db04dc225948f48ce44c6ce3ebc45428a426
TakamiChie <chie@onpu-tamago.net>
2012-10-05 09:12:46 +0900

    使用していないクラスを削除

--- AdbViewer/src/jp/takamichie/desktop/AdbViewer/AboutDialogPanel.java
+++
@@ -1,8 +0,0 @@
-package jp.takamichie.desktop.AdbViewer;
-
-import javax.swing.JPanel;
-
-public final class AboutDialogPanel extends JPanel {
-
-	private static final long serialVersionUID = 1L;
-}
